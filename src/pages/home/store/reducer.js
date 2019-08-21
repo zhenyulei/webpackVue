0ff0607cc90f464070d4c35644fdcf7cf6aaae85
zhenyulei <zhenyulei@jd.com>
2019-08-21 11:30:52 +0800

    修改redux

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -5,15 +5,17 @@ const defaultState = {
     myname:'lihua'
 };
 export default (state=defaultState,action)=>{
-    if(action.type === CHANGE_INPUT_VSLUE){
-        let newState1 = JSON.parse(JSON.stringify(state));
-        newState1.inputValue = action.value;
-        return newState1;
-    }else if(action.type === INITDATA){
-        let newState2 = JSON.parse(JSON.stringify(state));
-        console.log(action.initData);
-        newState2.list = action.initData;
-        return newState2;
+    switch(action.type){
+        case CHANGE_INPUT_VSLUE:
+            let newState1 = JSON.parse(JSON.stringify(state));
+            newState1.inputValue = action.value;
+            return newState1;
+        case INITDATA:
+            let newState2 = JSON.parse(JSON.stringify(state));
+            console.log(action.initData);
+            newState2.list = action.initData;
+            return newState2;
+        default:
+        return state;
     }
-    return state;
 }
